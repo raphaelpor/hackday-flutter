379e3532c4b7f474c130d540d7541acfb234efd8
João Fernando <joaofernando.costa@corp.globo.com>
2019-11-07 15:05:57 -0300

    Add button styles

--- lib/components/Button.dart
+++ lib/components/Button.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 Widget button() {
   return MaterialButton(
-    child: Text('Open Stories'),
+    child: Text(
+      'Open Stories',
+      style: TextStyle(
+        fontSize: 20,
+      ),
+    ),
     onPressed: () => print('pressed!'),
+    color: Colors.blueAccent,
+    textColor: Colors.white,
+    height: 50,
   );
 }
