9e86969596c036e5d5d4e15781d3888c7ebba7c6
João Fernando <joaofernando.costa@corp.globo.com>
2019-11-08 11:52:20 -0300

    Add map to iterate in stories array

--- lib/components/renderStories.dart
+++ lib/components/renderStories.dart
@@ -3,6 +3,10 @@ import 'package:hackday_stories/components/story/Story.dart';
 
 import 'storiesNav.dart';
 
+renderStory(slides) {
+  return slides.map<Widget>((item) => Text(item['legend'])).toList();
+}
+
 Widget renderStories(context, snapshot) {
   if (snapshot.hasData) {
     final data = snapshot.data;
@@ -12,11 +16,7 @@ Widget renderStories(context, snapshot) {
     return Stack(
       children: [
         Stack(
-          children: [
-            Story(true),
-            Story(false),
-            Story(false),
-          ],
+          children: renderStory(data.slides),
         ),
         storiesNav(),
       ],
