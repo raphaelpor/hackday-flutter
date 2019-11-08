de1fb64c9ea77f17a614049bbd79685db47cf45e
Raphael Porto <raphael.freitas@corp.globo.com>
2019-11-08 16:42:35 -0300

    Adiciona label de abrir conteúdo

--- lib/components/navComponents/openContent.dart
+++ lib/components/navComponents/openContent.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -23,6 +21,34 @@ Widget openContent(String url) {
       navigate(url);
     },
     child: Container(
+      child: Stack(
+        children: <Widget> [
+          Padding(
+            child: Align(
+              child: Icon(
+                Icons.keyboard_arrow_up,
+                color: Colors.white,
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+              alignment: Alignment.topCenter,
+            ),
+            padding: EdgeInsets.only(bottom: 10.0),
+          ),
+          Padding(
+            child: Center(
+              child: Text(
+                'Abrir',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+            ),
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+        ],
+      ),
       height: 50,
       color: Colors.transparent,
     ),
