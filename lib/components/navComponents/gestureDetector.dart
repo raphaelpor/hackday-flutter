4779e129c4308f4dcf12c1ce3ded59783efc6fd2
Raphael Porto <raphael.freitas@corp.globo.com>
2019-11-08 14:29:17 -0300

    Atualiza gesture detector

--- lib/components/navComponents/gestureDetector.dart
+++ lib/components/navComponents/gestureDetector.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-Widget gestureDetector(onTap) {
+Widget gestureDetector(
+  onTap,
+  onLongPressStart,
+  onLongPressEnd,
+) {
   return Expanded(
     child: GestureDetector(
       onTap: onTap,
-      onLongPressStart: (_) {
-        print('onLongPressStart');
-      },
-      onLongPressEnd: (_) {
-        print('onLongPressEnd');
-      },
+      onLongPressStart: onLongPressStart,
+      onLongPressEnd: onLongPressEnd,
       child: Container(color: Colors.transparent),
       // padding: EdgeInsets.all(0.0),
     ),
--- lib/components/storiesNav.dart
+++ lib/components/storiesNav.dart
@@ -3,14 +3,27 @@ import 'package:flutter/material.dart';
 import './navComponents/gestureDetector.dart';
 import './navComponents/openContent.dart';
 
-Widget storiesNav() {
+Widget storiesNav(
+  onNextPressed,
+  onPreviusPressed,
+  onLongPressStart,
+  onLongPressEnd,
+) {
   return Column(
     children: <Widget>[
       Expanded(
         child: Row(
           children: <Widget>[
-            gestureDetector(() => print('tap 1')),
-            gestureDetector(() => print('tap 2')),
+            gestureDetector(
+              onPreviusPressed,
+              onLongPressStart,
+              onLongPressEnd,
+            ),
+            gestureDetector(
+              onNextPressed,
+              onLongPressStart,
+              onLongPressEnd,
+            )
           ],
         ),
       ),
